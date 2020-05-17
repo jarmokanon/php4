8fd636861c0e8927963589ef7caae4165b04914f
jarmokanon <62425744+jarmokanon@users.noreply.github.com>
2020-05-17 21:33:58 +0200

    Create opdracht2.php

---
+++ opdracht2.php
@@ -0,0 +1,14 @@
+<?php
+class Car {
+  public $motor, $color, $bands;
+  function getInfo() {
+    return "This Car has an $this->motor motor and his color is $this->color.";
+  }
+}
+
+$detail = new Car();
+$detail->color = "blue";
+$detail->motor = "V12";
+echo $detail->getInfo();
+
+?>
