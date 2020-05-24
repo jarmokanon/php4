7b9d527fc02f02732c4b6b73205c32e38b9a5bf8
jarmokanon <62425744+jarmokanon@users.noreply.github.com>
2020-05-24 22:03:10 +0200

    derde opdracht

--- opdracht2.php
+++ opdracht2.php
@@ -1,6 +1,27 @@
 <?php
+/**
+ * src/opdracht2.php
+ * [Car description]
+ */
 class Car {
-  public $motor, $color, $bands;
+  /**
+   * [$motor]
+   *
+   * @var [string]
+   */
+  public $motor;
+  /**
+   * [$color]
+   *
+   * @var [string]
+   */
+  public $color;
+  /**
+   * [$bands]
+   *
+   * @var [string]
+   */
+  public $bands;
   function getInfo() {
     return "This Car has an $this->motor motor and his color is $this->color.";
   }
